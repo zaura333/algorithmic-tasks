e8f8bafe01fa8d23304599937451c2914b869f5b
Laura Wrońska <138938051+zaura333@users.noreply.github.com>
2024-11-21 19:20:13 +0100

    Fix and optimize 21.11.2024 solutions

--- 21-Nov-2024/solution.ts
+++ 21-Nov-2024/solution.ts
@@ -1,37 +1,39 @@
+// 2257. Count Unguarded Cells in the Grid
+
 function countUnguarded(m: number, n: number, guards: number[][], walls: number[][]): number {
-    const wallStrings: string[] = walls.map(wall => wall.join(""));
-    const guarded = new Set<string>(wallStrings);
+    const wallSet: Set<string> = new Set(walls.map(wall => wall.join(",")));
+    const guardSet = new Set<string>(guards.map(guard => guard.join(",")));
+    const guarded = new Set<string>();
 
     for (let g of guards) {
         let x = g[0];
         let y = g[1];
 
-        while (x < m && wallStrings.indexOf(`${x}${y}`) === -1) {
-            guarded.add(`${x}${y}`);
+        x = g[0] + 1;
+        while (x < m && !wallSet.has(`${x},${y}`) && !guardSet.has(`${x},${y}`)) {
+            guarded.add(`${x},${y}`);
             x++;
         }
 
-        x = g[0];
-
-        while (x >= 0 && wallStrings.indexOf(`${x}${y}`) === -1) {
-            guarded.add(`${x}${y}`);
+        x = g[0] - 1;
+        while (x >= 0 && !wallSet.has(`${x},${y}`) && !guardSet.has(`${x},${y}`)) {
+            guarded.add(`${x},${y}`);
             x--;
         }
 
         x = g[0];
-
-        while (y < n && wallStrings.indexOf(`${x}${y}`) === -1) {
-            guarded.add(`${x}${y}`);
+        y = g[1] + 1;
+        while (y < n && !wallSet.has(`${x},${y}`) && !guardSet.has(`${x},${y}`)) {
+            guarded.add(`${x},${y}`);
             y++;
         }
 
-        y = g[1];
-
-        while (y >= 0 && wallStrings.indexOf(`${x}${y}`) === -1) {
-            guarded.add(`${x}${y}`);
+        y = g[1] - 1;
+        while (y >= 0 && !wallSet.has(`${x},${y}`) && !guardSet.has(`${x},${y}`)) {
+            guarded.add(`${x},${y}`);
             y--;
         }
     }
 
-    return m*n - guarded.size;
-};
+    return m * n - guarded.size - wallSet.size - guardSet.size;
+}
